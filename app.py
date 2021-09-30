95d05297f9041b42e0f149c5e9ee521bfa56e46d
ngolos <53008687+ngolos@users.noreply.github.com>
2021-09-30 17:05:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,19 @@ from apps import home, data, model # import your app modules here
 
 app = MultiApp()
 st.set_page_config(page_title="Pets Supplements", page_icon="🐾", layout="wide")
+st.markdown(""" <style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style> """, unsafe_allow_html=True)
+
+padding = 0
+st.markdown(f""" <style>
+    .reportview-container .main .block-container{{
+        padding-top: {padding}rem;
+        padding-right: {padding}rem;
+        padding-left: {padding}rem;
+        padding-bottom: {padding}rem;
+    }} </style> """, unsafe_allow_html=True)
 st.markdown("""
 # Multi-Page App
 This multi-page app is using the [streamlit-multiapps](https://github.com/upraneelnihar/streamlit-multiapps) framework developed by [Praneel Nihar](https://medium.com/@u.praneel.nihar). Also check out his [Medium article](https://medium.com/@u.praneel.nihar/building-multi-page-web-app-using-streamlit-7a40d55fa5b4).
