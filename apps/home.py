d8a2facd4010e3eeff1102add5b2c6641aec86d5
ngolos <53008687+ngolos@users.noreply.github.com>
2021-09-30 16:59:12 +0200

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -47,3 +47,50 @@ def app():
     with col3:
         col3.metric(label="🐱Cats", value=(f"$ {total_sales_cats:,} Mln"))
     st.write("---")
+    a=df.groupby(['Sub-Category'])[['Mo_Revenue_Mln']].sum().sort_values('Mo_Revenue_Mln', ascending=False).reset_index()
+    sub_category_list = a['Sub-Category'].tolist()
+
+
+
+    st.header('Explore Sales by Target, Brand and Product')
+    target_type = df['Type'].drop_duplicates()
+    target_choice = st.multiselect('Select your target from 3 options:', options=sorted(target_type), default='Dogs')
+
+    #Filter df based on selection
+    filterd_type_df = df[df['Type'].isin(target_choice)]
+    #filterd_type_df
+    by_group=filterd_type_df.groupby(['Type', 'Sub-Category'])[['Mo_Revenue_Mln']].sum().sort_values(['Type','Mo_Revenue_Mln'], ascending=[False,False]).round(2).reset_index()
+    cat2=filterd_type_df.groupby(['Type', 'Brand']).agg(Sales_Mln=('Mo_Revenue_Mln', 'sum')).sort_values(by="Sales_Mln", ascending=False).head(15).reset_index()
+    cat3=filterd_type_df.groupby(['Type', "Brand", 'Product Name']).agg(Sales_Mln=('Mo_Revenue_Mln', 'sum')).sort_values(by="Sales_Mln", ascending=False).head(50).reset_index()
+    #by_group
+    chart=alt.Chart(by_group).mark_bar().encode(
+        x=alt.X('Mo_Revenue_Mln:Q'),
+        y=alt.Y("Sub-Category:N", sort=['Fish Oil Supplements','Probiotics','Multivitamins','Herbal Supplements','Antioxidants','Misc','Amino Acids']),
+        #column='Sub-Category',
+        color=('Type:N'),
+        tooltip=('Type','Mo_Revenue_Mln'),
+        #facet=alt.Facet('Sub-Category:N', columns=4, sort=sub_category_list),
+    ).properties(height=300)
+
+    chart2=alt.Chart(cat2).mark_bar().encode(
+        x=alt.X('Sales_Mln:Q'),
+        y=alt.Y("Brand:N",sort='-x'),
+        #column='Sub-Category',
+        color=('Type:N'),
+        tooltip=('Type','Sales_Mln'),
+        #facet=alt.Facet('Sub-Category:N', columns=4, sort=sub_category_list),
+    ).properties(height=300)
+
+
+
+    col1, col2, col3 = st.columns((1,1,2))
+    with col1:
+        #col1.metric(label="Dogs", value="%.2f" % total_sales_dogs)
+        st.subheader("Sales Split by Functionality:")
+        st.altair_chart(chart)
+    with col2:
+        st.subheader("Top Brands:")
+        st.altair_chart(chart2)
+    with col3:
+        st.subheader("Top Products:")
+        st.dataframe(cat3.head(10))
