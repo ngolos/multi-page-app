1b44751c12bc981f252a79afbce8563db45ed046
ngolos <53008687+ngolos@users.noreply.github.com>
2021-09-30 16:57:02 +0200

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -24,3 +24,26 @@ def app():
     df = get_data()
 
     st.write(df)
+    st.header('Page 1 - Explore the Largest Ingredient Groups for each Product Form')
+    #st.dataframe(df)
+
+    # TOP KPI's
+    total_sales_all = df["Mo_Revenue_Mln"].sum().round(2)
+    total_sales_dogs = df.query('Type=="Dogs"')["Mo_Revenue_Mln"].sum().round(2)
+    total_sales_cats = df.query('Type=="Cats"')["Mo_Revenue_Mln"].sum().round(2)
+    total_sales_catsdogs = df.query('Type=="Cats&Dogs"')["Mo_Revenue_Mln"].sum().round(2)
+    #average_rating = round(df_selection["Rating"].mean(), 1)
+    #star_rating = ":star:" * int(round(average_rating, 0))
+    #average_sale_by_transaction = round(df_selection["Total"].mean(), 2)
+    st.write("---")
+    st.header(f'Total Sales (30 days) Amz Best Sellers:$ {total_sales_all:,} Mln')
+    #  with st.echo():
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        #col1.metric(label="Dogs", value="%.2f" % total_sales_dogs)
+        col1.metric(label="🐶Dogs", value=(f"$ {total_sales_dogs:,} Mln"))
+    with col2:
+        col2.metric(label="🐶🐱Cats&Dogs", value=(f"$ {total_sales_catsdogs:,} Mln"))
+    with col3:
+        col3.metric(label="🐱Cats", value=(f"$ {total_sales_cats:,} Mln"))
+    st.write("---")
