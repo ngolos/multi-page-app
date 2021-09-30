79f43d90baac3056a294733a14291c8314e6f6c5
ngolos <53008687+ngolos@users.noreply.github.com>
2021-09-30 17:03:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from multiapp import MultiApp
 from apps import home, data, model # import your app modules here
 
 app = MultiApp()
-
+st.set_page_config(page_title="Pets Supplements", page_icon="🐾", layout="wide")
 st.markdown("""
 # Multi-Page App
 This multi-page app is using the [streamlit-multiapps](https://github.com/upraneelnihar/streamlit-multiapps) framework developed by [Praneel Nihar](https://medium.com/@u.praneel.nihar). Also check out his [Medium article](https://medium.com/@u.praneel.nihar/building-multi-page-web-app-using-streamlit-7a40d55fa5b4).
