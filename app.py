af7517e7b7775ab6d7427212c15b9b14c215ba57
ngolos <53008687+ngolos@users.noreply.github.com>
2021-10-05 12:43:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.markdown(""" <style>
 footer {visibility: hidden;}
 </style> """, unsafe_allow_html=True)
 
-padding = 2
+padding = 1
 st.markdown(f""" <style>
     .reportview-container .main .block-container{{
         padding-top: {padding}rem;
